110187d643a25a11d3abdcda48878f3fc0c2cd33
Francisco Alvarez <faove@hotmail.com>
2019-06-06 13:36:57 -0400

    Sistema de Facturacion, Ventas, Compras, Inventario

---
+++ app/Categoria.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table='categoria';
+
+    protected $primaryKey='idcategoria';
+
+    public $timestamps=false;
+
+    protected
+}
