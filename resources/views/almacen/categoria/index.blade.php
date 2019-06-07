2890832311f997ce944a61fd7ba627b25ac93ee8
Francisco Alvarez <faove@hotmail.com>
2019-06-06 22:28:41 -0400

    Mostrando la categorias en el sistema

--- resources/views/almacen/categoria/index.blade.php
+++ resources/views/almacen/categoria/index.blade.php
@@ -2,10 +2,34 @@
 @section('contenido')
 	<div class="row">
 		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12" >
-			<h3>Listado de Categoria <a href="categoria/create"><button>Nuevo</button></a> </h3>
-
-
-			
+			<h3>Listado de Categoria <a href="categoria/create"><button class="btn btn-success">Nuevo</button></a> </h3>
+			@include('almacen.categoria.search')			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+			<div class="table-responsive">
+				<table class="table table-striped table-bordered table-condensed table-hover">
+					<thead>
+						<th>Id</th>
+						<th>Nombre</th>
+						<th>Descripcion</th>
+						<th>Opciones</th>
+					</thead>
+					@foreach ($categorias as $cat)
+						<tr>
+							<td>{{$cat->idcategoria}}</td>
+							<td>{{$cat->nombre}}</td>
+							<td>{{$cat->descripcion}}</td>
+							<td>
+								<a href=""><button class="btn btn-info">Editar</button></a>
+								<a href=""><button class="btn btn-danger">Eliminar</button></a>
+							</td>
+						</tr>
+					@endforeach	
+				</table>
+			</div>
+			{{$categorias->render()}}
 		</div>
 	</div>
 @endsection
