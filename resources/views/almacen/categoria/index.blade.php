3fde02167b347c279411f2c31362d47a1127bc83
Francisco Alvarez <faove@hotmail.com>
2019-06-06 21:57:27 -0400

    Cree una nueva vista llamada search.blade.php

--- resources/views/almacen/categoria/index.blade.php
+++ resources/views/almacen/categoria/index.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.admin')
 @section('contenido')
-	<h3>Index</h3>
-@stop
+	<div class="row">
+		<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12" >
+			<h3>Listado de Categoria <a href="categoria/create"><button>Nuevo</button></a> </h3>
+
+
+			
+		</div>
+	</div>
+@endsection
