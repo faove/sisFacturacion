68a8e2ea8ed3ca34304ef74d58f345af89aad0de
Francisco Alvarez <faove@hotmail.com>
2019-06-12 10:45:28 -0400

    Modificaciones al modulo Request ArticuloFormRequest edit y ArticuloController

---
+++ app/Articulo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+	protected $table='articulo';
+
+    protected $primaryKey='idarticulo';
+
+    public $timestamps=false;
+
+    protected $fillable =[
+    	'idcategoria',
+    	'codigo',
+    	'nombre',
+    	'stock',
+    	'descripcion',
+    	'imagen',
+    	'estado'
+    ];
+
+    protected $guard =[
+    	
+    ];
+}
